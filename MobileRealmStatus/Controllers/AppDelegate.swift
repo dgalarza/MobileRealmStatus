4a1ff1446b22c8c981beba76cd919acabc180dfa
Damian Galarza <damian@thoughtbot.com>
2015-05-04 22:18:35 -0400

    Initial Commit

---
+++ MobileRealmStatus/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  MobileRealmStatus
+//
+//  Created by Damian Galarza on 5/4/15
+//  Copyright (c) 2015 dgalarza. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}

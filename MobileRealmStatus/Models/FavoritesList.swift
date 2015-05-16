b29563cb65c5ad864e4b0a4195933a811b88db97
Damian Galarza <damian@thoughtbot.com>
2015-05-15 22:34:08 -0400

    Remove Foundation imports where they aren't needed

--- MobileRealmStatus/Models/FavoritesList.swift
+++ MobileRealmStatus/Models/FavoritesList.swift
@@ -1,12 +1,3 @@
-//
-//  FavoritesList.swift
-//  MobileRealmStatus
-//
-//  Created by Damian Galarza on 5/15/15.
-//  Copyright (c) 2015 dgalarza. All rights reserved.
-//
-
-import Foundation
 import UIKit
 
 class FavoritesList {
--- MobileRealmStatus/RealmsApi.swift
+++ MobileRealmStatus/RealmsApi.swift
@@ -1,4 +1,3 @@
-import Foundation
 import Runes
 import Argo
 

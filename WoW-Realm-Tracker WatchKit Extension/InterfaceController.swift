ce5fce17fbe1b8a2e8510ce594800113ff9068ab
Damian Galarza <damian@thoughtbot.com>
2015-08-20 12:38:05 -0400

    Use `setImageNamed` to correctly display image

--- WoW-Realm-Tracker WatchKit Extension/InterfaceController.swift
+++ WoW-Realm-Tracker WatchKit Extension/InterfaceController.swift
@@ -24,13 +24,11 @@ class InterfaceController: WKInterfaceController {
         self.realms = ["Arthas", "Skullcrusher"]
         realmsTable.setNumberOfRows(realms.count, withRowType: "Realm")
 
-        let availableImage = UIImage(named: "Available")
-
         var index: Int;
         for index = 0; index < realms.count; index++ {
             let row = realmsTable.rowControllerAtIndex(index) as! RealmRowController
             row.nameLabel.setText(realms[index])
-            row.statusImage.setImage(availableImage)
+            row.statusImage.setImageNamed("Available")
         }
     }
 }

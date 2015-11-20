b625f2a633620037b04cd2f8175596c08ac35cb4
Damian Galarza <damian@thoughtbot.com>
2015-11-20 11:38:45 -0500

    Remove `ApplicationController`

--- WoW-Realm-Tracker/Controllers/AppDelegate.swift
+++ WoW-Realm-Tracker/Controllers/AppDelegate.swift
@@ -2,14 +2,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var controller = ApplicationController()
     lazy var window: UIWindow? = {
         return UIWindow.ApplicationWindow
     }()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         AppearanceManager.applyCustomAppearance()
-        window?.rootViewController = controller.rootViewController
+        window?.rootViewController = RootViewController()
         return true
     }
 }
--- WoW-Realm-Tracker/Controllers/ApplicationController.swift
+++
@@ -1,7 +0,0 @@
-struct ApplicationController {
-    var rootViewController: UIViewController {
-        let vc = RootViewController()
-        vc.controller = RealmsController(realmsDelegate: vc)
-        return vc
-    }
-}
